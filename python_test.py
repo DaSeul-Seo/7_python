cead6c17f2a9d77899482ec7624df1e6c7ade1fa
DaSeul-Seo <67898022+DaSeul-Seo@users.noreply.github.com>
2020-08-07 10:31:07 +0900

    Initial commit

---
+++ python_test.py
@@ -0,0 +1,14 @@
+a=10
+b=1
+print(a+b)
+
+var = 10 # var라는 변수에 10을 저장
+print(var) # 10출력. 변수에 10을 저장
+print(var) # 10출력
+print(10) # 10출력, 상수 10을 print()로 출력
+
+var1 = 10
+var2 = 20
+print(var1) # 10출력
+var1 = var2
+print(var1) # 20출력
